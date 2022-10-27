15ad0aaf2c2906e543376ab79f0e066f3b250050
ArtusC <9479656+ArtusC@users.noreply.github.com>
2022-10-27 08:04:38 -0300

    First change

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -9,32 +9,32 @@ func assertCorrectMessage(t testing.TB, got, want string) {
 	}
 }
 
-func TestHello(t *testing.T){
-	t.Run("saying hello to people in English", func(t *testing.T){
+func TestHello(t *testing.T) {
+	t.Run("saying hello to people in English", func(t *testing.T) {
 		got := Hello("Artus", "English")
 		want := "Hello, super world and Artus!"
 		assertCorrectMessage(t, got, want)
-		
+
 	})
-	t.Run("saying hello to people in Portugues", func(t *testing.T){
+	t.Run("saying hello to people in Portugues", func(t *testing.T) {
 		got := Hello("Artus", "Portugues")
 		want := "Olá, super mundo e Artus!"
 		assertCorrectMessage(t, got, want)
 	})
-	t.Run("say 'Hello, world and anything!' when an empty string is supplied in English" , func(t *testing.T){
+	t.Run("say 'Hello, world and anything!' when an empty string is supplied in English", func(t *testing.T) {
 		got := Hello("", "English")
 		want := "Hello, super world and anything!"
 		assertCorrectMessage(t, got, want)
 	})
-	t.Run("say 'Hello, world and anything!' when an empty string is supplied in Portugues" , func(t *testing.T){
+	t.Run("say 'Hello, world and anything!' when an empty string is supplied in Portugues", func(t *testing.T) {
 		got := Hello("", "Portugues")
 		want := "Hello, super world and anything!"
 		assertCorrectMessage(t, got, want)
 	})
-	t.Run("say 'Hello, world and anything!' when name and language are supplied" , func(t *testing.T){
+	t.Run("say 'Hello, world and anything!' when name and language are supplied", func(t *testing.T) {
 		got := Hello("", "")
 		want := "Hello, super world and anything!"
 		assertCorrectMessage(t, got, want)
 	})
-	
+
 }
