32440569bc17178dac5373f6fb596331ee903023
Artus Crusué Andermann <artus.andermann@neoway.com.br>
2023-11-14 09:04:52 -0300

    added sync chapter

---
+++ sync/v2/sync_test.go
@@ -0,0 +1,50 @@
+package sync
+
+import (
+	"sync"
+	"testing"
+)
+
+func TestCounter(t *testing.T) {
+	t.Run("incrementing the counter 3 time leaves it at 3", func(t *testing.T) {
+		counter := NewCounter()
+		counter.Increment()
+		counter.Increment()
+		counter.Increment()
+
+		assertCounter(t, counter, 3)
+
+	})
+
+	t.Run("it runs safely concurrently", func(t *testing.T) {
+
+		wantedCount := 1000
+		counter := NewCounter()
+
+		var wg sync.WaitGroup
+		wg.Add(wantedCount)
+
+		for i := 0; i < wantedCount; i++ {
+			go func() {
+				counter.Increment()
+				wg.Done()
+			}()
+		}
+		wg.Wait()
+
+		// Use channels when passing ownership of data
+		// Use mutexes for managing state
+		// go vet
+		// 	Remember to use go vet in your build scripts as it can alert you to some
+		// 	subtle bugs in your code before they hit your poor users.
+
+		assertCounter(t, counter, wantedCount)
+	})
+}
+
+func assertCounter(t testing.TB, got *Counter, want int) {
+	t.Helper()
+	if got.Value() != want {
+		t.Errorf("got %d, but expected %d", got.Value(), want)
+	}
+}
